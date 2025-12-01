b2294e2535ede08c97a923fa6b052a7c122729f7
Tiffa256 <tiffashan10@gmail.com>
2025-12-01 16:16:10 +0800

    Update AdminUsers.js

--- src/admin/AdminUsers.js
+++ src/admin/AdminUsers.js
@@ -1,1 +1,9 @@
+export default function AdminUsers() {
+  return (
+    <div className="admin-container">
+      <h2 className="admin-title">Users</h2>
+      <p>Here you will manage user accounts.</p>
+    </div>
+  );
+}
 
