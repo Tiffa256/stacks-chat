c885d9eef2d7272d5f976b3b3bd68034609d92a0
Tiffa256 <tiffashan10@gmail.com>
2025-12-04 21:50:39 +0800

    Update AdminMessages.js

--- src/admin/AdminMessages.js
+++ src/admin/AdminMessages.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { ref, onValue } from "firebase/database";
 import { db } from "../firebase";
 import { useNavigate } from "react-router-dom";
-import "./admin.css";
+import "./Admin.css";
 
 export default function AdminMessages() {
   const [users, setUsers] = useState([]);
